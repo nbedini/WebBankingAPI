1bfcbd0d3ca9ec2e286d0d9775a9b28ea00211d5
Nikolas Bedini <nikolasbedini04@gmail.com>
2021-11-23 11:35:14 +0100

    Login e Logout Funzionanti con token JWT

---
+++ WebBankingAPI/Controllers/loginController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using WebBankingAPI.Models;
+
+namespace WebBankingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class loginController : ControllerBase
+    {
+        [HttpGet("")]
+        public ActionResult Login([FromBody]User usercredential)
+        {
+            using (WebBankingContext model = new WebBankingContext())
+            {
+                User candidate = model.Users.FirstOrDefault(fod => fod.Username == usercredential.Username && fod.Password == usercredential.Password);
+                if (candidate == null) return Ok("Login non effettuato, controlla username o password");
+
+                var TokenHandler = new JwtSecurityTokenHandler();
+                var TokenDescriptor = new SecurityTokenDescriptor
+                {
+                    SigningCredentials = new SigningCredentials(SecurityKeyGenerator.GetSecurityKey(), SecurityAlgorithms.HmacSha256Signature),
+                    Expires = DateTime.UtcNow.AddDays(1),
+                    Subject = new ClaimsIdentity(
+                        new Claim[]
+                        {
+                            new Claim("Id", candidate.Id.ToString()),
+                            new Claim("Username", candidate.Username),
+                        }
+                    )
+                };
+
+                SecurityToken token = TokenHandler.CreateToken(TokenDescriptor);
+                candidate.LastLogin = DateTime.Now;
+                model.SaveChanges();
+                return Ok(TokenHandler.WriteToken(token));
+
+            }
+        }
+    }
+}
---
+++ WebBankingAPI/Controllers/logoutController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using WebBankingAPI.Models;
+
+namespace WebBankingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class logoutController : ControllerBase
+    {
+        [HttpGet("")]
+        public ActionResult Logout()
+        {
+            var username = HttpContext.User.Claims.FirstOrDefault(fod => fod.Type == "Username").Value;
+            var id = Convert.ToInt32(HttpContext.User.Claims.FirstOrDefault(fod => fod.Type == "Id").Value);
+            using (WebBankingContext model = new WebBankingContext())
+            {
+                var candidate = model.Users.FirstOrDefault(fod => fod.Username == username && fod.Id == id);
+                candidate.LastLogout = DateTime.Now;
+                model.SaveChanges();
+                return Ok("Logout effettuato con successo");
+            }
+        }
+    }
+}
